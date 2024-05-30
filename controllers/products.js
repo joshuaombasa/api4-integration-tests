57dff6b21d5c8cbcca2b3fd615949e9a34299306
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-30 23:54:12 +0300

    initial commit -TDD

--- controllers/products.js
+++ controllers/products.js
@@ -5,10 +5,19 @@ const Product = require('../models/product')
 
 productsRouter.get('/', async (request, response, next) => {
   try {
-    
+    const products = await Product.find({})
+    response.send(products)
   } catch (error) {
     next(error)
   }
 })
+
+// productsRouter.get('/', async (request, response, next) => {
+//   try {
+
+//   } catch (error) {
+//     next(error)
+//   }
+// })
 
 module.exports = productsRouter
