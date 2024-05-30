037ddfe39b37a568c0e211c1cd3c162d5da77845
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-30 23:53:37 +0300

    initial commit -TDD

---
+++ tests/test-helper.js
@@ -0,0 +1,26 @@
+const Product = require('../models/product')
+
+const productsList = [
+  {
+    title: 'product1',
+    price: 10
+  },
+  {
+    title: 'product2',
+    price: 20
+  },
+  {
+    title: 'product3',
+    price: 30
+  },
+  {
+    title: 'product4',
+    price: 40
+  },
+  {
+    title: 'product15',
+    price: 50
+  },
+]
+
+module.exports = { productsList }
