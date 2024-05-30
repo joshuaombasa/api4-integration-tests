cca1d2bdb9c8fa0eb45e27700dff25cba1440a29
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-30 23:53:15 +0300

    initial commit -TDD

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('./app')
 const config = require('./utils/config')
+const logger = require('./utils/logger')
 
 app.listen(config.PORT, () => {
-  console.log(`server running on port ${config.PORT}`)
+  logger.info(`server running on port ${config.PORT}`)
 })
